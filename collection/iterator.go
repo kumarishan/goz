b4f2096d5c7b488ebb6e781f473269ffeac37900
Kumar Ishan <kumar.ishan89@gmail.com>
2022-09-27 23:31:52 +0530

    removed dead code

--- collection/iterator.go
+++ collection/iterator.go
@@ -1,36 +1,11 @@
 package collection
 
-////// Iterator //////
+type Iterable[A any] interface {
+	Iterator() Iterator[A]
+}
 
 type Iterator[A any] interface {
 	HasNext() bool
 	Next() (A, error)
 	Iterator() Iterator[A]
-}
-
-type IteratorOps[A any, B any] struct {
-	Iterator[A]
-}
-
-func (i IteratorOps[A, B]) Filter(op func(A) bool) Iterator[A] {
-	return nil
-}
-
-//// Iterable /////
-
-type Iterable[A any] interface {
-	Iterator() Iterator[A]
-}
-
-type IterableOps[A any, B any, C Iterable[A], CB Iterable[B], F IterableFactory[B, CB]] interface {
-	Iterable[A]
-
-	Filter(func(A) bool) C
-	Map(func(A) B) CB
-	FlatMap(func(A) Iterable[B]) CB
-}
-
-type IterableFactory[A any, C Iterable[A]] interface {
-	From(source Iterable[A]) C
-	Empty() C
 }
